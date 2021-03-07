7a915b3568ae1752f67752e60fbd7eb6d7bd7b87
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-03-07 15:37:03 -0600

    added seed data to mysql folder

--- assets/mysql/ctc_seeds.sql
+++ assets/mysql/ctc_seeds.sql
@@ -1,4 +1,41 @@
+-- department
+INSERT INTO department(name)
+VALUES("Hematology/Oncology"), ("Radiation Oncology"), ("Front Office"), ("Billing"), ("Administration");
 
+-- role
+INSERT INTO role(title, salary, deptid)
+VALUES("RN (Hem/Onc)", "75000.00", 1);
 
+INSERT INTO role(title, salary, deptid)
+VALUES("RN (Rad/Onc)", "85000.00", 2);
+
+INSERT INTO role(title, salary, deptid)
+VALUES("MD (Hem/Onc)", "350000.00", 1);
+
+INSERT INTO role(title, salary, deptid)
+VALUES("MD (Rad/Onc)", "420000.00", 2);
+
+INSERT INTO role(title, salary, deptid)
+VALUES("MA", "35000.00", 1);
+
+INSERT INTO role(title, salary, deptid)
+VALUES("MA", "35000.00", 2);
+
+INSERT INTO role(title, salary, deptid)
+VALUES("F/O Coordinator", "27000.00", 3);
+
+INSERT INTO role(title, salary, deptid)
+VALUES("A/R Specialist", "45000.00", 4);
+
+INSERT INTO role(title, salary, deptid)
+VALUES("Coding Specialist", "54000.00", 4);
+
+INSERT INTO role(title, salary, deptid)
+VALUES("Manager", "65000.00", 5);
+
+INSERT INTO role(title, salary, deptid)
+VALUES("Practice Administrator", "70000.00", 5);
+
+--employee
 
 
